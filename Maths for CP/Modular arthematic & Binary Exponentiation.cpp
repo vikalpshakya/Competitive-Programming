b9201c94a5cd9a176fc7f83295cfdecc46f32653
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2024-03-27 13:45:04 +0530

    Added file

--- Maths for CP/Modular arthematic & Binary Exponentiation.cpp
+++ Maths for CP/Modular arthematic & Binary Exponentiation.cpp
@@ -6,13 +6,16 @@
 
 */
 
+// Binary Exponentiation : https://cp-algorithms.com/algebra/binary-exp.html
+
 #include<bits/stdc++.h> 
 using namespace std;
 #define int long long
 
 // Time Complexity : O(log(b)) [ b is the power of a]
 
-// This is to find : ( A ^ B ) % N , Where A and B are very large numbers , so inorder to prevent integer ovelflow , we do  binary exponentation.
+// This is to find : ( A ^ B )  , Where A and B are very large numbers 
+// Here we are finding (A^B) % N 
 
 int binExpo(int a, int b, int n) {
     if(b == 0) return 1;
