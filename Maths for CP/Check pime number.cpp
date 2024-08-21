2cb023ecbc523b53bf2ecb80e10881f566e0f463
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2024-08-21 14:57:12 +0530

    Added file

--- Maths for CP/Check pime number.cpp
+++ Maths for CP/Check pime number.cpp
@@ -19,6 +19,22 @@ bool isPrime(int n) {
     return count == 2;
 }
 
+// TC = root n
+
+// intution : Every prime number can be expressed in the form 6k+1;
+
+bool is_prime(int n){
+    if(n<=1) return 0;
+    if(n<=3) return 1;
+    if(n%2==0 || n%3==0) return 0;
+    
+    for(int i=5; i<sqrt(n); i+=6){
+        if(n%i || n%i+2) return 0;
+    }
+    return 1;
+}
+
+
 int main() {
     int n; cin >> n;
     cout << (isPrime(n) ? "YES\n" : "NO\n" ) ;
